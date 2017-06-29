04526f7071a70367e8d5d8d5d606596df340ec63
PYERSIK <danilgakhov@gmail.com>
2017-06-29 11:28:01 +0300

    first commit

---
+++ app/src/main/java/com/example/danich/mygit_pr02/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.danich.mygit_pr02;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import static com.example.danich.mygit_pr02.R.styleable.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
